a028eb5b2c270ca8821d47216d2070d2a2f26350
tyzbit <tyzbit@gmail.com>
2015-11-24 13:22:08 -0500

    fix broken script
    it was broken, now it is fixed
    
    i am awesome

--- wanup.sh
+++ wanup.sh
@@ -4,7 +4,7 @@
 
 if [ ! -s /tmp/dlhosts ] ; then
 
-echo -e "#!/bin/sh\n(wget -O - http://winhelp2002.mvps.org/hosts.txt ; wget -O - http://jamesisbored.com/iphone/content/hosts.php ; wget -O - http://adaway.org/hosts.txt ; wget -O - http://www.malwaredomainlist.com/hostslist/hosts.txt ; wget -O - http://www.mvps.org/winhelp2002/hosts.txt ; wget -O - http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&star) | grep .0.0. | grep -Fvf /tmp/whitelist | sed -e '2,\$s/127.0.0.1/0.0.0.0/g' -e 's/[[:space:]]*#.*$//' > /etc/dnsmasq/hosts/blkhosts\nlogger 'DOWNLOADED ADBLOCK HOSTS FILE(s)'\nkillall -1 dnsmasq" > /tmp/dlhosts
+echo -e "#!/bin/sh\n(wget -O - http://winhelp2002.mvps.org/hosts.txt ; wget -O - http://jamesisbored.com/iphone/content/hosts.php ; wget -O - http://adaway.org/hosts.txt ; wget -O - http://www.malwaredomainlist.com/hostslist/hosts.txt ; wget -O - http://www.mvps.org/winhelp2002/hosts.txt ; wget -O - 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&star') | grep .0.0. | grep -Fvf /tmp/whitelist | sed -e '2,\$s/127.0.0.1/0.0.0.0/g' -e 's/[[:space:]]*#.*$//' > /etc/dnsmasq/hosts/blkhosts\nlogger 'DOWNLOADED ADBLOCK HOSTS FILE(s)'\nkillall -1 dnsmasq" > /tmp/dlhosts
 chmod 777 /tmp/dlhosts
 sleep 120
 /tmp/dlhosts
