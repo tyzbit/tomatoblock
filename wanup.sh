b3e9a09f2ab41b301e1e83b28a112f4012ccfbd7
tyzbit <tyzbit@gmail.com>
2016-03-09 18:51:09 -0500

    added additional hosts to blacklist
    temporary fix until I can either make a hosted blacklist for myself or
    these hosts are integrated into StevenBlack's list

--- wanup.sh
+++ wanup.sh
@@ -1,10 +1,55 @@
 # Whitelist sites
-#cat > /tmp/whitelist <<EOF
-#EOF
+cat > /tmp/whitelist <<EOF
+EOF
+
+cat > /tmp/blacklist <<EOF
+0.0.0.0 p2pcamweb.tutk.com
+0.0.0.0 tutk.com
+0.0.0.0 www.tutk.com
+0.0.0.0 ac3.msn.com
+0.0.0.0 choice.microsoft.com
+0.0.0.0 choice.microsoft.com.nsatc.net
+0.0.0.0 compatexchange.cloudapp.net
+0.0.0.0 corp.sts.microsoft.com
+0.0.0.0 corpext.msitadfs.glbdns2.microsoft.com
+0.0.0.0 cs1.wpc.v0cdn.net
+0.0.0.0 diagnostics.support.microsoft.com
+0.0.0.0 feedback.search.microsoft.com
+0.0.0.0 feedback.windows.com
+0.0.0.0 i1.services.social.microsoft.com
+0.0.0.0 i1.services.social.microsoft.com.nsatc.net
+0.0.0.0 oca.telemetry.microsoft.com
+0.0.0.0 oca.telemetry.microsoft.com.nsatc.net
+0.0.0.0 pre.footprintpredict.com
+0.0.0.0 redir.metaservices.microsoft.com
+0.0.0.0 services.wes.df.telemetry.microsoft.com
+0.0.0.0 settings-sandbox.data.microsoft.com
+0.0.0.0 sls.update.microsoft.com.akadns.net
+0.0.0.0 sqm.df.telemetry.microsoft.com
+0.0.0.0 sqm.telemetry.microsoft.com
+0.0.0.0 sqm.telemetry.microsoft.com.nsatc.net
+0.0.0.0 ssw.live.com
+0.0.0.0 statsfe1.ws.microsoft.com
+0.0.0.0 statsfe2.update.microsoft.com.akadns.net
+0.0.0.0 survey.watson.microsoft.com
+0.0.0.0 telecommand.telemetry.microsoft.com
+0.0.0.0 telecommand.telemetry.microsoft.com.nsatc.net
+0.0.0.0 telemetry.urs.microsoft.com
+0.0.0.0 vortex-bn2.metron.live.com.nsatc.net
+0.0.0.0 vortex-cy2.metron.live.com.nsatc.net
+0.0.0.0 vortex-sandbox.data.microsoft.com
+0.0.0.0 vortex-win.data.microsoft.com
+0.0.0.0 vortex.data.microsoft.com
+0.0.0.0 watson.live.com
+0.0.0.0 watson.microsoft.com
+0.0.0.0 watson.ppe.telemetry.microsoft.com
+0.0.0.0 watson.telemetry.microsoft.com
+0.0.0.0 watson.telemetry.microsoft.com.nsatc.net
+EOF
 
 if [ ! -s /tmp/dlhosts ] ; then
 
-echo -e "#!/bin/sh\n(wget -O - http://winhelp2002.mvps.org/hosts.txt ; wget -O - http://jamesisbored.com/iphone/content/hosts.php ; wget -O - http://adaway.org/hosts.txt ; wget -O - http://www.malwaredomainlist.com/hostslist/hosts.txt ; wget -O - http://www.mvps.org/winhelp2002/hosts.txt ; wget -O - 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&star') | grep .0.0. | grep -Fvf /tmp/whitelist | sed -e '2,\$s/127.0.0.1/0.0.0.0/g' -e 's/[[:space:]]*#.*$//' > /etc/dnsmasq/hosts/blkhosts\nlogger 'DOWNLOADED ADBLOCK HOSTS FILE(s)'\nkillall -1 dnsmasq" > /tmp/dlhosts
+echo -e "#!/bin/sh\n(wget -O - https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts; wget -O - http://jamesisbored.com/iphone/content/hosts.php; cat /tmp/blacklist) | grep .0.0. | grep -Fvf /tmp/whitelist | sed -e '2,\$s/127.0.0.1/0.0.0.0/g' -e 's/[[:space:]]*#.*$//' > /etc/dnsmasq/hosts/blkhosts\nlogger 'DOWNLOADED ADBLOCK HOSTS FILE(s)'\nkillall -1 dnsmasq" > /tmp/dlhosts
 chmod 777 /tmp/dlhosts
 sleep 120
 /tmp/dlhosts
